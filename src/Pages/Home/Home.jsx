360aa6eed2744d04e17b476c057983d54795c339
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-04 14:34:00 -0700

    Feed:: Setting up

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Feed from '../../Components/Feed/Feed'
 
 export default function Home({ sidebar }) {
   return (
     <>
       <Sidebar sidebar={sidebar}/>
+
+      <div className={`container ${sidebar ? "" : 'large-container'}`}>
+        <Feed />
+      </div>
     </>
   )
 }
