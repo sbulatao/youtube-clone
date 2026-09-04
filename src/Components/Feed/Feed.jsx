10bee872ae7f194dbd9907abd43f5b9da04fe1ba
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-04 15:42:36 -0700

    PlayVideo:: Added and setting upwhen first card is clicked in Feed

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Feed.css'
+import { Link } from 'react-router-dom'
 import thumbnail1 from '../../assets/thumbnail1.png'
 import thumbnail2 from '../../assets/thumbnail2.png'
 import thumbnail3 from '../../assets/thumbnail3.png'
@@ -13,12 +14,15 @@ export default function Feed() {
   return (
     <div className="feed">
 
-        <div className='card'>
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel to learn coding that help you to be a web developer</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
+        <Link to={`/video/20/4521`} className='card'>
+            <div className='card'>
+                <img src={thumbnail1} alt="" />
+                <h2>Best channel to learn coding that help you to be a web developer</h2>
+                <h3>Greatstack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+        </Link>
+        
         <div className='card'>
             <img src={thumbnail2} alt="" />
             <h2>Best channel to learn coding that help you to be a web developer</h2>
