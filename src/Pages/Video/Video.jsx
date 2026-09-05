87424832b78898eadb93418d5a95130b6968c6ff
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-04 17:51:17 -0700

    Recommended .jsx &.css:: Setup and styled

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
+import Recommended from '../../Components/Recommended/Recommended'
 
 export default function Video() {
   return (
     <div className='play-container'>
       <PlayVideo />
+      <Recommended />
     </div>
   )
 }
