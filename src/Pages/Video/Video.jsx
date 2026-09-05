baf1ef0f79a5cb74db731b84227251ebcc69a624
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-04 22:51:03 -0700

    PlayVideo:: using useParams for videoId when video is clicked

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 import Recommended from '../../Components/Recommended/Recommended'
+import { useParams } from 'react-router-dom'
 
 export default function Video() {
+
+  const { videoId, categoryId } = useParams();
+
   return (
     <div className='play-container'>
-      <PlayVideo />
+      <PlayVideo videoId={videoId}/>
       <Recommended />
     </div>
   )
