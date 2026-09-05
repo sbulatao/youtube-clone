3025d690ff714c50cafe3393a377b00d3a9094a3
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-05 00:37:39 -0700

    PlayVideo:: Added fetch video data

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './PlayVideo.css'
 import video1 from '../../assets/video.mp4'
 import like from '../../assets/like.png'
@@ -7,21 +7,36 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { API_KEY, valueConverter } from '../../data'
+import axios from 'axios'
+import moment from 'moment'
 
 export default function PlayVideo({ videoId }) {
 
+    const [apiData, setApiData] = useState(null);
+    const [channelData, setChannelData] =useState(null);
+
+    async function fetchVideoData() { // fetching video data
+        const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`)
+        setApiData(data.items[0]);
+    }
+
+    useEffect(() => {
+        fetchVideoData();
+    }, [])
 
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted></video> */}
-        <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
-        <h3>Best Youtube Channel To Learn Web Development</h3>
+        <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
+
         <div className="play-video-info">
-            <p>1525 Views &bull; 2 days ago</p>
+            <p>{valueConverter(apiData ? apiData.statistics.viewCount : "0K")} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "0 hours ago"}</p>
             <div>
-                <span><img src={like} alt="" /> 420</span>
-                <span><img src={dislike} alt="" /> 69</span>
+                <span><img src={like} alt="" /> {apiData ? valueConverter(apiData.statistics.likeCount) : 0}</span>
+                <span><img src={dislike} alt="" /> </span>
                 <span><img src={share} alt="" /> Share</span>
                 <span><img src={save} alt="" /> Save</span>
             </div>
@@ -32,17 +47,16 @@ export default function PlayVideo({ videoId }) {
         <div className="publisher">
             <img src={jack} alt="" />
             <div>
-                <p>GreatStack</p>
+                <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
                 <span>1M Subscribers</span>
             </div>
             <button>Subscribe</button>
         </div>
 
         <div className="vid-description">
-            <p>Channel that makes learning Easy</p>
-            <p>Suscribe GreateStack to watch More Tutorials on web development</p>
+            <p>{apiData ? apiData.snippet.description.slice(0, 250) : "Description Here"}</p>
             <hr />
-            <h4>130 Comments</h4>
+            <h4>{apiData ? valueConverter(apiData.statistics.commentCount) : 102} Comments</h4>
 
             <div className="comment">
                 <img src={user_profile} alt="" />
