31c4e9adc65f76f86bd7b7dc5192908e11a2b1e5
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-09-06 13:03:08 -0700

    PlayVideo: moved functions in useEffect, added if statement only after apiData becomes available to fetch channel data and added ? after apiData/channelData  to display output

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -14,26 +14,53 @@ import moment from 'moment'
 export default function PlayVideo({ videoId }) {
 
     const [apiData, setApiData] = useState(null);
-    const [channelData, setChannelData] =useState(null);
+    const [channelData, setChannelData] = useState(null);
 
-    async function fetchVideoData() { // fetching video data
-        const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`)
-        setApiData(data.items[0]);
-    }
-
+    // 1. fetch video data when componenet mounts or videoId changes
     useEffect(() => {
-        fetchVideoData();
-    }, [])
+        async function fetchVideoData() { // fetching video data
+            try {
+                const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`);
+                setApiData(data.items[0]);
+                console.log("video data: ",data.items[0])
+            } catch (error) {
+                console.error("ERROR FETCHING VIDEO DATA: ", error)
+            }
+        }
+
+        if(videoId) fetchVideoData();
+    }, [videoId]) // Add videoId HERE so it refetches if user switches videos
+
+
+    // 2. fetch channel data ONLY after apiData becomes available
+    useEffect(() => {
+        // STOP if apiData or channelId isn't loaded yet
+        if(!apiData || !apiData.snippet?.channelId) return;
+
+        async function fetchOtherData() { // fetching channel data
+            try {
+                const { data } = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`);
+                setChannelData(data.items[0]);
+                console.log("channel data: ", data.items[0]);
+                
+            } catch (error) {
+                console.log("ERROR FETCHING CHANNEL DATA: ", error)
+            }
+        }
+
+        fetchOtherData();
+    },[apiData]) // watches apiData and starts as soon as it updates
+
 
   return (
     <div className='play-video'>
         {/* <video src={video1} controls autoPlay muted></video> */}
         <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
-        <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
+        <h3>{apiData ? apiData?.snippet?.title : "Title Here"}</h3>
 
         <div className="play-video-info">
-            <p>{valueConverter(apiData ? apiData.statistics.viewCount : "0K")} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "0 hours ago"}</p>
+            <p>{valueConverter(apiData ? apiData?.statistics?.viewCount : "0K")} Views &bull; {apiData ? moment(apiData?.snippet?.publishedAt).fromNow() : "0 hours ago"}</p>
             <div>
                 <span><img src={like} alt="" /> {apiData ? valueConverter(apiData.statistics.likeCount) : 0}</span>
                 <span><img src={dislike} alt="" /> </span>
@@ -47,14 +74,15 @@ export default function PlayVideo({ videoId }) {
         <div className="publisher">
             <img src={jack} alt="" />
             <div>
-                <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
-                <span>1M Subscribers</span>
+                {/* <p>{apiData ? apiData.snippet.channelTitle : ""}</p> */}
+                <p>{channelData ? channelData?.snippet?.title : ""}</p>
+                <span>{channelData ? channelData?.statistics?.subscriberCount : "1M"} Subscribers</span>
             </div>
             <button>Subscribe</button>
         </div>
 
         <div className="vid-description">
-            <p>{apiData ? apiData.snippet.description.slice(0, 250) : "Description Here"}</p>
+            <p>{apiData ? apiData?.snippet?.description.slice(0, 250) : "Description Here"}</p>
             <hr />
             <h4>{apiData ? valueConverter(apiData.statistics.commentCount) : 102} Comments</h4>
 
